eb0d857eca3570418c964c0107ea43ff8d36704b
Gabiru-cpu <gabriel_librande@hotmail.com>
2023-05-09 11:46:20 -0300

    Initial

---
+++ MySqlContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {         
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>();
+        }
+    }
+}
---
+++ Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
